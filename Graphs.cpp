c382db64162385807b553e7269fca06d174bb130
Jairo Calcina <j.calcina.v@gmail.com>
2024-10-07 01:40:13 -0300

    feat: add example in main function

--- Graphs.cpp
+++ Graphs.cpp
@@ -1,6 +1,34 @@
 #include <iostream>
+#include "Graph.h"
+
+// Dependencias para identificar memory leaks
+//#define _CRTDBG_MAP_ALLOC
+//#include <stdlib.h>
+//#include <crtdbg.h>
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	{
+		Graph* graph = new Graph(5);
+
+		graph->add_edge(0, 1);
+		graph->add_edge(0, 2);
+		graph->add_edge(1, 2);
+		graph->add_edge(1, 3);
+		graph->add_edge(2, 4);
+		graph->add_edge(3, 4);
+
+		graph->print_graph();
+
+		graph->remove_edge(0, 1);
+		graph->remove_edge(1, 2);
+		graph->remove_edge(2, 4);
+
+		graph->print_graph();
+
+		delete graph;
+	}
+
+	//_CrtSetReportMode(_CRT_WARN, _CRTDBG_MODE_DEBUG);
+	//_CrtDumpMemoryLeaks();
 }
