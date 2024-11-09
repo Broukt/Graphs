5ec7d97731304e4586e4eebcfcb69ac9383e709d
Jairo Calcina <j.calcina.v@gmail.com>
2024-11-09 13:21:35 -0300

    feat: add another example to main function

--- Graphs.cpp
+++ Graphs.cpp
@@ -9,14 +9,24 @@
 int main()
 {
 	{
-		Graph* graph = new Graph(5);
+		Graph* graph = new Graph(9);
 
-		graph->add_edge(0, 1);
-		graph->add_edge(0, 2);
-		graph->add_edge(1, 2);
-		graph->add_edge(1, 3);
-		graph->add_edge(2, 4);
-		graph->add_edge(3, 4);
+		graph->add_edge_undirected(0, 1);
+		graph->add_edge_undirected(0, 7);
+		graph->add_edge_undirected(1, 2);
+		graph->add_edge_undirected(1, 7);
+		graph->add_edge_undirected(2, 3);
+		graph->add_edge_undirected(2, 8);
+		graph->add_edge_undirected(2, 5);
+		graph->add_edge_undirected(3, 4);
+		graph->add_edge_undirected(3, 5);
+		graph->add_edge_undirected(4, 5);
+		graph->add_edge_undirected(5, 6);
+		graph->add_edge_undirected(6, 7);
+		graph->add_edge_undirected(6, 8);
+		graph->add_edge_undirected(7, 8);
+
+		graph->dijkstra(0);
 
 		graph->print_graph();
 
